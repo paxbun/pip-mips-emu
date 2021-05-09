b59064c05bd1b1856235369bdef18466b9b1781b
Chanjung Kim <freiyer.paxbun@gmail.com>
2021-05-09 18:24:57 +0900

    Initial commit

---
+++ Source/Lib.cc
@@ -0,0 +1,9 @@
+// Copyright (c) 2021 Chanjung Kim (paxbun). All rights reserved.
+// Licensed under the MIT License.
+
+#include <pip-mips-emu/Lib.hh>
+
+double Add(double lhs, double rhs) noexcept
+{
+    return lhs + rhs;
+}
---
+++ Source/Main.cc
@@ -0,0 +1,4 @@
+// Copyright (c) 2021 Chanjung Kim (paxbun). All rights reserved.
+// Licensed under the MIT License.
+
+int main(int argc, char* argv[]) {}
